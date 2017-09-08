9b4170ae2a1cc15a097ec2e678e605891bcc31b7
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2017-09-09 05:03:16 +0900

    connected to db.. very tired...

--- docker/container/php-artisan/script/script.php
+++ docker/container/php-artisan/script/script.php
@@ -3,11 +3,22 @@
 $file = file_get_contents('/var/www/html/.env.example');
 
 $file = preg_replace('/DB_HOST=127.0.0.1/', 'DB_HOST=mysql', $file);
-$file = preg_replace('/DB_DATABASE=homestead/', 'DB_DATABASE=HarekazeCTF', $file);
-$file = preg_replace('/DB_USERNAME=homestead/', 'DB_USERNAME=${MYSQL_USER}', $file);
-$file = preg_replace('/DB_PASSWORD=secret/', 'DB_DATABASE=${MYSQL_PASSWORD}', $file);
+$file = preg_replace('/DB_DATABASE=homestead/', 'DB_DATABASE='.getenv('MYSQL_DATABASE'), $file);
+$file = preg_replace('/DB_USERNAME=homestead/', 'DB_USERNAME='.getenv('MYSQL_USER'), $file);
+$file = preg_replace('/DB_PASSWORD=secret/', 'DB_PASSWORD='.getenv('MYSQL_PASSWORD'), $file);
 
 file_put_contents ('/var/www/html/.env', $file);
 
+while(true)
+{
+    try
+    {
+        $dbh = new PDO('mysql:host=mysql;dbname=HarekazeCTF', getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'));
+        $dbh = null;
+        break;
+    }
+    catch (PDOException $e) { sleep(3); }
+
+}
 echo shell_exec('cd /var/www/html/ && php artisan key:generate');
-echo shell_exec('cd /var/www/html/ && artisan migrate');
+echo shell_exec('cd /var/www/html/ && php artisan migrate');
