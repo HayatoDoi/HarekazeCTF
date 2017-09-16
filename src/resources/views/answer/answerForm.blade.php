eb9e785d1d4f265b40ae67e2b4ccdd5579ec4b2f
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2017-09-16 18:59:48 +0900

    Fixed bug when null

--- src/resources/views/answer/answerForm.blade.php
+++ src/resources/views/answer/answerForm.blade.php
@@ -36,7 +36,7 @@
   @endif
     {{ csrf_field() }}
     <div class="form-group">
-      <input type="text" class="form-control" name="aFlag" placeholder="HarekazeCTF{******************}">
+      <input type="text" class="form-control" name="aFlag" placeholder="HarekazeCTF{******************}" required >
     </div>
     <button type="submit" class="btn btn-primary">送信</button>
   </form>
