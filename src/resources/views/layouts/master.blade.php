89b26f8f561581d87bfdf5ef85b5fd8cd4ccd3f5
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2017-09-16 18:14:38 +0900

    Create question by markdown format

--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -27,7 +27,7 @@
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         {{--  end meta  --}}
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min.js"></script>
         <!-- Fonts -->
 
         <!-- Styles -->
@@ -100,5 +100,8 @@
       <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+      
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min.js"></script>
+      <script src="{{ asset('js/markdown.preview.js') }}"></script>
     </body>
 </html>
--- src/resources/views/master/createQuestions.blade.php
+++ src/resources/views/master/createQuestions.blade.php
@@ -23,9 +23,29 @@
     <label for="qFlag">フラグ</label>
     <input type="text" class="form-control" name="qFlag" value="">
   </div>
+
   <div class="form-group">
     <label for="qSentence">問題文</label>
-    <textarea class="form-control" name="qSentence" rows="8" value=""></textarea>
+    <div class="m-t-1 m-l-1 m-r-1" id="editor">
+      <!--タブのボタン部分-->
+      <ul class="nav nav-tabs" id="editor" role="tablist">
+        <li class="nav-item">
+          <a class="nav-link active" data-toggle="tab" href="#markdown" role="tab" aria-controls="markdown">markdown</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="tab" href="#preview" role="tab" aria-controls="preview">preview</a>
+        </li>
+      </ul>
+      <!--タブのコンテンツ部分-->
+      <div class="tab-content">
+        <div id="markdown" class="tab-pane active">
+          <textarea id="md-text" class="form-control" name="qSentence" rows="8"></textarea>
+        </div>
+        <div id="preview" class="tab-pane card">
+          <div id="md-preview" class="card-block"></div>
+        </div>
+      </div>
+    </div>
   </div>
   <button type="submit" class="btn btn-primary">作成</button>
 </form>
